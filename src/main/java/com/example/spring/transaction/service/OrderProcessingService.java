0065f93a1815f4268b49b055e310b1fd3bfdacc7
ITMASTER\MMF.Jan <mmf.jan@reative-almaviva.it>
2025-02-27 14:15:41 +0100

    transaction added

--- src/main/java/com/example/spring/transaction/service/OrderProcessingService.java
+++ src/main/java/com/example/spring/transaction/service/OrderProcessingService.java
@@ -4,6 +4,9 @@ import com.example.spring.transaction.entity.Order;
 import com.example.spring.transaction.entity.Product;
 import com.example.spring.transaction.exceptions.InsufficientStockException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class OrderProcessingService {
@@ -15,6 +18,7 @@ public class OrderProcessingService {
         this.inventoryService = inventoryService;
     }
 
+    @Transactional(propagation = Propagation.REQUIRED,isolation = Isolation.READ_COMMITTED)
     public Order placeOrder(Order newOrder) throws InsufficientStockException {
      // get the product from the inventory
      // validate stock availability
@@ -23,7 +27,7 @@ public class OrderProcessingService {
      // updated stock in inventory
         Product productRetrieved = inventoryService.getProduct(newOrder.getProductId());
         if(newOrder.getQuantity()> productRetrieved.getAvailableQuantity()){
-            throw new InsufficientStockException("Insuffecient stock availability");
+            throw new InsufficientStockException("Insufficient stock availability");
         }
         newOrder.setTotalPrice(newOrder.getQuantity() * productRetrieved.getPrice());
         orderService.saveOrder(newOrder);
